360f79e9f5644961cbff0884c6cd82f764f9df86
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-25 21:27:59 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const loadPhones = async () => {
+    const url = `https://openapi.programming-hero.com/api/phones?search=iphone`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhones(data.data);
+}
+
+const displayPhones = phones => {
+    phones.forEach(phone => {
+        
+    });
+}
+
+
+loadPhones();
