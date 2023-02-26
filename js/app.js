0aa308b5645b11534efe473567340138f6d0c1db
sawonhossain34 <sawonhossain3491@gmail.com>
2023-02-26 16:01:44 +0600

    modal add show details btn

--- js/app.js
+++ js/app.js
@@ -1,17 +1,27 @@
-const loadPhones = async (searchText) => {
+const loadPhones = async (searchText,dataLimit) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     const res = await fetch(url);
     const data = await res.json();
-    displayPhones(data.data);
+    displayPhones(data.data,dataLimit);
 }
 
-const displayPhones = phones => {
+const displayPhones = (phones,dataLimit) => {
     const phoneContainer = document.getElementById('phone-container');
     // new contant remove old content
     phoneContainer.innerHTML = '';
-    document.getElementById('search-field').value='';
-    // only 20 phone
-    phones = phones.slice(0,20);
+    // document.getElementById('search-field').value='';
+
+
+    // only 10 phone
+    const showAll = document.getElementById('show-all');
+    if(dataLimit && phones.length > 10){
+        phones = phones.slice(0,10);
+        showAll.classList.remove('d-none');
+    }
+    else{
+        showAll.classList.add('d-none');
+    }
+    
     // no phone found
     const noPhone = document.getElementById('no-phone-found');
     if(phones.length === 0){
@@ -30,7 +40,10 @@ const displayPhones = phones => {
                         <img src="${phone.image}" class="card-img-top" alt="...">
                         <div class="card-body">
                           <h5 class="card-title">${phone.phone_name}</h5>
-                          <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                          <p class="card-text">This is a longer card with supporting text    below as a natural lead-in to additional content. This content is a little bit longer.
+                          </p>
+                          <button onclick ="loadPhoneDetails('${phone.slug}')"  href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">Show Dtails</button>
+                          
                         </div>
                       </div>
         `;
@@ -43,16 +56,23 @@ const displayPhones = phones => {
 
 
 
-
-// button click search phone
-document.getElementById('btn-search').addEventListener('click',function(){
+const prossessSearch = (dataLimit) => {
     toggelSpener(true);
     const searchField =document.getElementById('search-field');
     const searchText = searchField.value;
-    loadPhones(searchText);
-    // console.log(searchText);
+    loadPhones(searchText,dataLimit);
+}
+// button click search phone
+document.getElementById('btn-search').addEventListener('click',function(){
+    prossessSearch(10);
 })
-
+// input field key enter event handelar
+document.getElementById('search-field').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        prossessSearch(10);
+      // code for enter
+    }
+});
 
 const toggelSpener = loading => {
     const displyLoader =document.getElementById('loader');
@@ -63,4 +83,30 @@ const toggelSpener = loading => {
     }
 }
 
-// loadPhones();
+// no the best way to the btn show all
+document.getElementById('btn-show-all').addEventListener('click',function(){
+    prossessSearch();
+})
+
+const loadPhoneDetails = async id => {
+    const url =`https://openapi.programming-hero.com/api/phone/${id}`
+    const res = await fetch(url);
+    const data = await res.json();
+    displayPhoneDetails(data.data);
+}
+
+const displayPhoneDetails = phone => {
+console.log(phone);
+const modalTitle = document.getElementById('exampleModalLabel');
+modalTitle.innerText=phone.name;
+const phoneDetails =document.getElementById('phone-details');
+phoneDetails.innerHTML =`
+<p>Release Date: ${phone.releaseDate ? phone.releaseDate : 'NO reeaseDate'}</p>
+<p>Stroage : ${phone.mainFeatures.storage ? phone.mainFeatures.storage : 'not found '}</p>
+<p>Others: ${phone.others ? phone.others.Bluetooth : 'NO bluetooth'}</p>
+`;
+}
+
+
+
+loadPhones('apple');
